f66b30f94b8b678db527e01da4c9c5eac13a284d
Gustavo Siqueira <gus@brid-it.com>
2021-06-13 15:27:34 -0300

    migrated from serverless-php

---
+++ src/Log/Loggers/EloquentLogger.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brid\Database\Log\Loggers;
+
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+use Brid\Database\Log\Handler\EloquentHandler;
+use Brid\Database\Log\Processor\ContextProcessor;
+use Brid\Database\Log\Processor\RequestProcessor;
+
+class EloquentLogger
+{
+
+  public function __invoke(array $config): LoggerInterface
+  {
+
+    $logger = new Logger('eloquent');
+    $logger->pushHandler(new EloquentHandler());
+    $logger->pushProcessor(new ContextProcessor());
+    $logger->pushProcessor(new RequestProcessor());
+
+    return $logger;
+
+  }
+
+}
---
+++ src/Log/Processor/RequestProcessor.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brid\Database\Log\Processor;
+
+class RequestProcessor
+{
+
+  public function __invoke(array $record): array
+  {
+
+    $request = request();
+
+    $record['app_id'] = $request->header('app-id');
+    $record['client_id'] = $request->getAttribute('oauth_client_id');
+    $record['customer_id'] = null;
+    $record['user_id'] = $request->getAttribute('oauth_user_id');
+
+    $url = $request->getUri()->getScheme() . '://' .
+      $request->getUri()->getHost() .
+      ($request->getUri()->getPort() ?? '') .
+      $request->getUri()->getPath() .
+      $request->getUri()->getQuery();
+
+    $record['extra']['server'] = $request->server('SERVER_ADDR');
+    $record['extra']['host'] = $request->getUri()->getHost();
+    $record['extra']['origin'] = $request->header('origin');
+    $record['extra']['uri'] = $request->getUri()->getPath();
+    $record['extra']['request'] = [
+      'client_ip' => $request->getClientIp(),
+      'http_method' => $request->getMethod(),
+      'headers' => $request->getHeaders(),
+      'params' => $request->all(),
+      'url' => $url,
+      'user_agent' => $request->getClientUserAgent(),
+    ];
+
+    return $record;
+  }
+
+}
